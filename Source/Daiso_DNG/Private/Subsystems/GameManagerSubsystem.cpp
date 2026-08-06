0d30156bcd655ab3776b0201eaa9f781306cd5f3
StarDust <86709115+StarDust504@users.noreply.github.com>
2026-08-07 01:36:26 +1000

    scoring

--- Source/Daiso_DNG/Private/Subsystems/GameManagerSubsystem.cpp
+++ Source/Daiso_DNG/Private/Subsystems/GameManagerSubsystem.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Subsystems/GameManagerSubsystem.h"
+#include "Daiso_DNG/Public/Data/DataStructs.h"
 
 FName UGameManagerSubsystem::AddComboToTempArray(int32 NumberToAppend)
 {
@@ -19,9 +20,54 @@ FName UGameManagerSubsystem::AddComboToTempArray(int32 NumberToAppend)
 	return FName(*TempString);
 }
 
+FName UGameManagerSubsystem::RemoveComboFromTempArray(int32 NumberToRemove)
+{
+	FString TempString;
+	
+	if (!TempScore.IsEmpty() && TempScore.Contains(NumberToRemove))
+	{
+		TempScore.RemoveSingle(NumberToRemove);
+		
+		TempScore.Sort();
+	}
+	
+	for (int32 ArrNum : TempScore)
+	{
+		TempString.Append(FString::FromInt(ArrNum));
+	}
+	
+	return FName(*TempString);
+}
+
 int32 UGameManagerSubsystem::GetCurrentScore(FName Combo)
 {
-	return 0;
+	if (!ScoreDataTable)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Cyan, "DT not found!!!");
+		return -1;
+	}
+		
+	
+	FComboData* FoundRow = ScoreDataTable->FindRow<FComboData>(Combo, "");
+	if (!FoundRow) return 0;
+	
+	return FoundRow->ComboScore;
+}
+
+void UGameManagerSubsystem::RegisterDice(ACPP_Dice* DiceToRegister)
+{
+	if (RegisteredDice.Contains(DiceToRegister))
+		return;
+	
+	RegisteredDice.Add(DiceToRegister);
+}
+
+void UGameManagerSubsystem::UnregisterDice(ACPP_Dice* DiceToUnregister)
+{
+	if (!RegisteredDice.Contains(DiceToUnregister))
+		return;
+	
+	RegisteredDice.Remove(DiceToUnregister);
 }
 
 
